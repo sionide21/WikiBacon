a1f764bcf194c56f59a22c0f5524e294b9aaf8f3
Ben Olive <sionide21@gmail.com>
2013-06-03 07:47:14 -0400

    Link canonicalization
    Does not include the special character escape code substitutions

--- parser/parser.rb
+++ parser/parser.rb
@@ -67,9 +67,37 @@ class Page
       @raw = raw
     end
 
+    def prefix
+      if prefix = raw[/^([^:]+):/, 1]
+        clean(prefix).capitalize
+      end
+    end
+
+    def page_name
+      capitalize_first(clean(raw[/^([^:]+:)?([^|]+)/, 2]))
+    end
+
+    def target
+      if prefix
+        "#{prefix}:#{page_name}"
+      else
+        page_name
+      end
+    end
+
     def internal?
       # Ignore portals for now
-      not raw.include? ':'
+      not prefix
+    end
+
+    private
+
+    def capitalize_first(text)
+      text.gsub(/^(.)/) { |l| l.upcase }
+    end
+
+    def clean(link)
+      link.gsub(/[\s_]+/, '_').sub(/^_/, '').sub(/_$/, '')
     end
   end
 end
